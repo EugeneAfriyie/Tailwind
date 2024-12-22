bf40036c2538b40c9d98dcf1d80fede2aaba9e3d
Eugene Afriyie <groupeight00@gmail.com>
2024-12-22 11:40:47 +0000

    fixes bugs

--- Nike/src/Components/Nav.jsx
+++ Nike/src/Components/Nav.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
-
-// Correctly import the SVG file
-import headerlogo from '../assets/images/headerlogo.svg';
-// import {hamburger} from '../assets/icons/hamburger.svg';
+import { headerLogo } from '../assets/images';
+import {navLinks} from '../Constants/index'
+import { hamburger } from '../assets/icons';
 
 const Nav = () => {
   return (
-    <header className='padding-x py-8 absolute z-10 w-full border border-gray-700 '>
-      <nav className='flex justify-between items-center max-container'>
+    <header className="padding-x py-8 absolute z-10 w-full border border-gray-700">
+      <nav className="flex justify-between items-center max-container">
         <a href="/">
-          <img src={headerlogo} alt="Logo" width={130} height={20}/>
+          <img src={headerLogo} alt="Logo" width={130} height={20} />
         </a>
 
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((link) => (
+            <li key={link.label}>
+              <a href={link.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
+                {link.label}
+              </a>
+            </li>
+          ))}
+
           
         </ul>
+
+        <div className="max-lg:block hidden">
+          <img src={hamburger} alt="" width={25} height={25}  />
+        </div>
       </nav>
     </header>
   );
