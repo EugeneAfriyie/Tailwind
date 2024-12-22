74d0dc43fc57813cd6f98ebfba6a62ecec832f3e
Eugene Afriyie <groupeight00@gmail.com>
2024-12-22 12:58:59 +0000

    created shoe card component

--- Nike/src/Components/Button.jsx
+++ Nike/src/Components/Button.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({label,iconUrl}) => {
   return (
-    <button className=''>
-        
+    <button className='flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white '>
+        {label}
+
+        <img src={iconUrl} alt="Arrow right " className='ml-2 rounded-full w-5 h-5' />
     </button>
   )
 }
