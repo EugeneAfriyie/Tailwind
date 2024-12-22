c2704f066ba39e0146ddf9f8d2e5b8fe759789de
Eugene Afriyie <groupeight00@gmail.com>
2024-12-22 10:08:43 +0000

    added the header image

--- Nike/src/Components/Nav.jsx
+++ Nike/src/Components/Nav.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 
 // Correctly import the SVG file
 import headerlogo from '../assets/images/headerlogo.svg';
+// import {hamburger} from '../assets/icons/hamburger.svg';
 
 const Nav = () => {
   return (
-    <header>
-      <nav>
+    <header className='padding-x py-8 absolute z-10 w-full border border-gray-700 '>
+      <nav className='flex justify-between items-center max-container'>
         <a href="/">
-          <img src={headerlogo} alt="Header Logo" />
+          <img src={headerlogo} alt="Logo" width={130} height={20}/>
         </a>
+
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          
+        </ul>
       </nav>
     </header>
   );
