6941b1032493b0169f591e63c1bb5cd6dd1e8907
Eugene Afriyie <groupeight00@gmail.com>
2024-12-23 06:12:57 +0000

    finished the superProduct section and started with service section

--- Nike/src/Components/PopularProductsCard.jsx
+++ Nike/src/Components/PopularProductsCard.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { star } from '../assets/icons'
 
-const PopularProductsCard = () => {
+const PopularProductsCard = ({imgURL, name,price}) => {
   return (
-    <div>PopularProductsCard</div>
+    <div className='flex flex-1 flex-col w-full max-sm:w-full '>
+        <img src={imgURL} alt={name} className='w-[280px] h-[280px]' />
+
+        <div className="mt-4 flex justify-start gap-2.5">
+            <img src={star} alt="rating" width={24} height={24} />
+
+            <p className="font-montserrat text-xl leading-normal to-slate-gray">(4.5)</p>
+        </div>
+
+        <h3 className='mt-0 text-2xl leading-normal font-semibold font-palanquin'>{name}</h3>
+        <p className='mt-0 font-semibold text-2xl leading-normal text-coral-red'>{price}</p>
+    </div>
+
   )
 }
 
--- Nike/src/section/SuperQuality.jsx
+++ Nike/src/section/SuperQuality.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import Button from '../Components/Button'
+import { shoe8 } from '../assets/images'
 
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section id='about-us'
+    className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'>
+
+      <div className="flex flex-1 flex-col">
+
+        <h2 className=' font-palanquin text-4xl capitalize lg:max-w-lg font-bold'> 
+
+            We Provide You  
+          <span className='text-coral-red '> Super  </span>
+
+           <span className='text-coral-red '>Quality </span> Shoes
+        </h2>
+
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Ensuring premium comfort and style , our maticulously crafted shoes are designed to last,prociding you with the best quality,innovation and comfort.
+        </p>
+
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence is what makes us stand out from the rest.
+        </p>
+        <div className="mt-11">
+
+        <Button label={'View Details'} />
+        </div>
+      </div>
+
+      <div className="flex-1 flex items-center justify-center">
+        <img src={shoe8} alt="shoe8" width={570} height={522} 
+        className='object-contain' />
+      </div>
+      
+    </section>
   )
 }
 
