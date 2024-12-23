977cc215b6728a4e81477d522529f058e2384894
Eugene Afriyie <groupeight00@gmail.com>
2024-12-23 19:51:15 +0000

    create ReviewCard

--- Nike/src/section/CustomerReviews.jsx
+++ Nike/src/section/CustomerReviews.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { reviews } from '../Constants'
+import ReviewCard from '../Components/ReviewCard'
 
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className='max-container'>
+      <h3 className='font-palanquin text-center text-4xl font-bold'>
+        What Our?
+        <span className='text-coral-red'> Customers </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto max-w-lg text-center">
+        Hear genuine stories from our customers about their experiences with our products and services.
+      </p>
+
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review, index) => (
+          <ReviewCard
+          
+          />
+        ))}
+      </div>
+    </section>
   )
 }
 
